e5de527644c9c736fb7d06c0333c6f805297f08f
Дмитрий Баев <itspodarok@mail.ru>
2024-11-03 20:36:09 +0300

    EnemyAiChanges
    Return Mode, Guard Mode changes. Atack mode create

--- AshesOfTheGods/Assets/Scripts/Enemy/EnemyMovement.cs
+++ AshesOfTheGods/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,66 +1,114 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private Transform player;
+    
     private Rigidbody2D rb;
     private Vector2 movement;
     [SerializeField] private float atackDistanse;
     private bool onAtackDistanse = false;
     private bool on_ground = false;
 
-    [SerializeField] private Transform guardedPoint;
-    private bool guardModeRightMove;
+    [SerializeField] private Transform firstGuardedPoint;
+    [SerializeField] private Transform secondGuardedPoint;
+    private bool guardModeRightMove = false;
     [SerializeField] private float guardDistance;
     private Vector2 returnMovement;
 
     private bool guardMode = true;
     private bool angryMode = false;
     private bool returnMode = false;
+    
+    private bool guardWaitMode = false;
+    private bool returnWaitMode = false;
+    private bool returnWaitModeFlag = false;
+    [SerializeField] float guardWaitTime;
+    [SerializeField] float targetLostTime;
 
+    private GameObject player;
+    private Transform playerTransform;
+    private PlayerCollisionState player_on_platform;
+
+    [SerializeField] float enemyDamage;
+    private bool enemyCanAtack = true;
+    [SerializeField] float atackCoodown;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        player = GameObject.FindWithTag("Player").transform;
+        player = GameObject.FindWithTag("Player");
+        playerTransform = player.transform;
+        player_on_platform = player.GetComponent<PlayerCollisionState>();
+        if (firstGuardedPoint.transform.position.x > secondGuardedPoint.transform.position.x)
+        {
+            Transform transform = firstGuardedPoint;
+            firstGuardedPoint = secondGuardedPoint;
+            secondGuardedPoint = transform;
+        }
+    }
+
+
+
+    private void Start()
+    {
+
+        StartCoroutine(WaitMode(guardWaitTime, targetLostTime, atackCoodown));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //�������� ��������� ��� �����
-        if (Vector2.Distance(player.position, transform.position) <= atackDistanse-1)
-            onAtackDistanse = true;
         
+        
+
+        Debug.DrawRay(new Vector3(firstGuardedPoint.position.x, -500, 0), new Vector3(0, 1000, 0));
+        Debug.DrawRay(new Vector3(secondGuardedPoint.position.x, -500, 0), new Vector3(0, 1000, 0));
+        //Atack Distance check
+        if (Vector2.Distance(playerTransform.position, transform.position) <= atackDistanse - 1)
+            onAtackDistanse = true;
+
         else
             onAtackDistanse = false;
 
-        //������ � ������� ������
-        movement = (player.position - transform.position).normalized;
+        //Vector towards the enemy 
+        movement = (playerTransform.position - transform.position).normalized;
         movement.y = 0;
 
-        //������ � ������� ���� ��������������
-        returnMovement = (guardedPoint.position - transform.position).normalized;
-        returnMovement.y = 0;
-
-        //��������� �� ������ � ���� ��������������
-        if (Vector2.Distance(rb.position, guardedPoint.position) < guardDistance + 0.1f && !angryMode)
+        //Vector towards the guarded point
+        if (Vector2.Distance(rb.position, firstGuardedPoint.position) < Vector2.Distance(rb.position, secondGuardedPoint.position))
+        {
+            returnMovement = (firstGuardedPoint.position - transform.position).normalized;
+            returnMovement.y = 0;
+        }
+        else
+        {
+            returnMovement = (secondGuardedPoint.position - transform.position).normalized;
+            returnMovement.y = 0;
+        }
+        //exit from return, entrance to guard
+        if (rb.position.x + transform.localScale.x >= firstGuardedPoint.position.x & rb.position.x- transform.localScale.x <= secondGuardedPoint.position.x & !angryMode)
         {
             guardMode = true;
             returnMode = false;
         }
 
-        //��������� �� ����� � ���� ��������������
-        if (Vector2.Distance(player.position,guardedPoint.position) < guardDistance)
+        //exit from guard, entrance to angry
+
+        if (playerTransform.position.x > firstGuardedPoint.position.x & playerTransform.position.x < secondGuardedPoint.position.x)
         {
             angryMode = true;
             guardMode = false;
         }
 
-        if (player.position.y >= -4f && angryMode)
+        //Debug.Log(player_on_platform.GetOnPlatform());
+        //exit from angry, entrance to return
+        if ((playerTransform.position.y - rb.position.y>0.1f) & angryMode & player_on_platform.GetOnPlatform())
         {
             returnMode = true;
+            returnWaitModeFlag = true;
             angryMode = false;
         }
     }
@@ -69,20 +117,25 @@ public class EnemyMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+     
         if (!onAtackDistanse & on_ground & angryMode)
-          AngryMode(movement);
+            AngryMode(movement);
+
+        else if (onAtackDistanse & angryMode & enemyCanAtack)
+            AtackMode();
 
         if (returnMode)
             ReturnMode();
 
         if (guardMode)
-            GuardMode(); 
+            GuardMode();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject collisionObject = collision.gameObject;
         if (collisionObject.CompareTag("Ground"))
             on_ground = true;
+  
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -90,34 +143,83 @@ public class EnemyMovement : MonoBehaviour
         GameObject collisionObject = collision.gameObject;
         if (collisionObject.CompareTag("Ground"))
             on_ground = false;
+        
     }
 
-    //�������� �� �������
+
     private void AngryMode(Vector2 movement)
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 
-    //���� ��������������
+
     private void GuardMode()
     {
-        if (rb.position.x > guardedPoint.position.x + guardDistance)
-            guardModeRightMove = false;
-        
-        else if (rb.position.x < guardedPoint.position.x - guardDistance)
-            guardModeRightMove = true;
-        
-        if (guardModeRightMove)
+        if (guardModeRightMove & !guardWaitMode)
             rb.MovePosition(rb.position + Vector2.right * speed * Time.fixedDeltaTime);
-        
-        else
-            rb.MovePosition(rb.position + Vector2.left*speed * Time.fixedDeltaTime);
+
+        if (!guardModeRightMove & !guardWaitMode)
+            rb.MovePosition(rb.position + Vector2.left * speed * Time.fixedDeltaTime);
+
+        if (rb.position.x + transform.localScale.x >= secondGuardedPoint.position.x & guardModeRightMove)
+        {
+            guardWaitMode = true;
+            guardModeRightMove = false;
+        }
+        else if (rb.position.x - transform.localScale.x <= firstGuardedPoint.position.x & !guardModeRightMove)
+        {
+            guardWaitMode = true;
+            guardModeRightMove = true;
+        }       
     }
 
-    //����������� � ���� ��������������
+
     private void ReturnMode()
     {
-            rb.MovePosition(rb.position + returnMovement * speed * Time.fixedDeltaTime);   
+        if (returnWaitModeFlag)
+        {
+            returnWaitMode = true;
+            returnWaitModeFlag = false;
+        }
+        if (!returnWaitMode)
+            rb.MovePosition(rb.position + returnMovement * speed * Time.fixedDeltaTime);
+
     }
 
+    private void AtackMode()
+    {
+        Debug.Log("Скелет бьёт");
+        
+        player.GetComponent<PlayerStats>().ReduceHp(enemyDamage);
+        enemyCanAtack = false;
+    }
+
+    private IEnumerator WaitMode(float guardWaitTime, float targetLostTime, float atackCoodown)
+    {
+        while (true)
+        {
+            if (guardWaitMode)
+            {
+                print("Ждёт");
+                yield return new WaitForSeconds(guardWaitTime);
+                guardWaitMode = false;
+            }
+            if (returnWaitMode)
+            {
+                print("Ждёт ретёрна");
+                yield return new WaitForSeconds(targetLostTime);
+                returnWaitMode = false;
+            }
+            if (!enemyCanAtack)
+            {
+                print("Кд атаки");
+                yield return new WaitForSeconds(atackCoodown);
+                enemyCanAtack = true;
+            }
+            yield return new WaitForFixedUpdate();
+
+            
+        }
+    }
 }
+
