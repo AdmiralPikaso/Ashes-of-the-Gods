2371474d17a7f803ad00207f791f2a9026df46e5
Дмитрий Баев <itspodarok@mail.ru>
2024-11-24 21:49:08 +0300

    Hp UI and  First skill Ui

--- AshesOfTheGods/Assets/Scripts/Enemy/Enemy.cs
+++ AshesOfTheGods/Assets/Scripts/Enemy/Enemy.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float hp;
-
+    public float HpNow { get; private set; }
+    public float HpMax { get; private set; }
+    private void Awake()
+    {
+        HpNow = hp;
+        HpMax = hp;
+    }
     public void TakeDamage(float damage)
     {
-        hp -= damage;
+        HpNow -= damage;
         if (hp <= 0)
         {
             Die();
