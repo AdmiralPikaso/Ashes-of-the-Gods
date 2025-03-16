82bbbcc53b35be432f830c34fa0959f6c54cab5a
Дмитрий Баев <itspodarok@mail.ru>
2025-03-16 21:45:27 +0300

    Settings Done

--- AshesOfTheGods/Assets/Scripts/GameUI/SetResolutionScript.cs
+++ AshesOfTheGods/Assets/Scripts/GameUI/SetResolutionScript.cs
@@ -5,11 +5,62 @@ using TMPro;
 
 public class SetResolutionScript : MonoBehaviour
 {
-    
+   
 
     [SerializeField] private TMP_Dropdown resoulutionDP;
     [SerializeField] private Toggle fullScreenToggle;
-    
+
+    private void Start()
+    {
+        bool fullScreen = Screen.fullScreen;
+        Screen.fullScreen = true;
+
+        int width = Screen.currentResolution.width;
+        int height = Screen.currentResolution.height;
+
+
+        if (width == 1280 & height == 720)
+            resoulutionDP.value = 0;
+
+        else if (width == 1366 & height == 768)
+            resoulutionDP.value = 1;
+        
+        else if (width == 1600 & height == 900)
+            resoulutionDP.value = 2;
+        
+        else if (width == 1920 & height == 1080)
+            resoulutionDP.value = 3;
+        
+        else if (width == 2160 & height == 1080)
+            resoulutionDP.value = 4;
+        
+        else if (width == 2560 & height == 1440)
+            resoulutionDP.value = 5;
+        
+        else if (width == 2560 & height == 1600)
+            resoulutionDP.value = 6;
+        
+        else if (width == 2960 & height == 1664)
+            resoulutionDP.value = 7;
+        
+        else if (width == 2960 & height == 1440)
+            resoulutionDP.value = 8;
+        
+        else if (width == 3024 & height == 1964)
+            resoulutionDP.value = 9;
+        
+        else if (width == 3200 & height == 1800)
+            resoulutionDP.value = 10;
+        
+        else if (width == 3456 & height == 2234)
+            resoulutionDP.value = 11;
+        
+        else if (width == 3840 & height == 2160)
+            resoulutionDP.value = 12;
+
+        
+        fullScreenToggle.isOn = fullScreen;
+    }
     public void ChangeResolution()
     {
         switch (resoulutionDP.value)
