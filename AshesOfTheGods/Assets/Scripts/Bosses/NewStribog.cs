fca0aefd8e503ee2369a4afa174e67ec0a1bb149
Иван Латыговский <latygovskii@sfedu.ru>
2025-05-04 18:21:22 +0300

    first lvl sounds

--- AshesOfTheGods/Assets/Scripts/Bosses/NewStribog.cs
+++ AshesOfTheGods/Assets/Scripts/Bosses/NewStribog.cs
@@ -3,16 +3,31 @@ using UnityEngine.Audio;
 
 public class NewStribog : Enemy
 {
+    [Header("Hit Sounds")]
+    [SerializeField] private AudioClip[] hitSounds;
+    [SerializeField] private float hitSoundVolume;
+    public AudioSource audioS;
     private void Awake()
     {
+        audioS = gameObject.AddComponent<AudioSource>();
         animator = GetComponent<Animator>();
         HpNow = hp;
         HpMax = hp;
     }
 
+     private int lastSoundIndex = -1;
+    private void PlayRandomHitSound()
+    {
+        int randomIndex = Random.Range(0, hitSounds.Length);
+        if (randomIndex == lastSoundIndex)
+            randomIndex = Random.Range(0, hitSounds.Length);
+        else
+            Sounds.StaticSound(hitSounds[randomIndex], audioS, hitSoundVolume);
+        lastSoundIndex = randomIndex;
+    }
     public override void TakeDamage(float damage)
     {
-        Sounds.Sound(damageSound, GetComponent<StribogScript>().audioSource, volume, minPitch, maxPitch);
+        PlayRandomHitSound();
         HpNow -= damage;
         gameObject.GetComponent<StribogScript>().CountAttack();
         if (HpNow <= 0)
